9b59578d43cea58f598fc2aff9c84d9d2f992597
zengguirong <18520788695@163.com>
2017-05-09 11:39:21 +0800

    订单创建页面状态字段采用下拉列表的方式

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,7 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+	@order_status=Const::ORDER_STATUS
   end
 
   # GET /orders/1/edit
--- app/models/const.rb
+++ app/models/const.rb
@@ -4,4 +4,5 @@ class Const
   PASSWORD_ERROR_LIMIT = 3
   USER_LEVELS = (1..3)
   DEFAULT_USER_LEVEL = 1
+  ORDER_STATUS=[['已询价', '00A'], ['已报价', '00B'], ['已确定', '00C']]
 end
