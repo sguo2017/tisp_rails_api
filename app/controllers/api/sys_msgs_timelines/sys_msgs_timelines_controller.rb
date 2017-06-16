359b7d3d6682b0d36cb83fbb99b1f2d66a76f70c
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-06-16 09:58:48 +0800

    创建订单数量限制

--- app/controllers/api/sys_msgs_timelines/sys_msgs_timelines_controller.rb
+++ app/controllers/api/sys_msgs_timelines/sys_msgs_timelines_controller.rb
@@ -49,16 +49,16 @@ class Api::SysMsgsTimelines::SysMsgsTimelinesController < ApplicationController
   # PATCH/PUT /sys_msgs_timelines/1
   # PATCH/PUT /sys_msgs_timelines/1.json
   def update
+    token = params[:token].presence
+    user = token && User.find_by_authentication_token(token.to_s)
     respond_to do |format|
-      if avaliable_orders_to_add(user) < 0
+     if avaliable_orders_to_add(user) < 0
         format.json {
            render json: {status: -1, msg:"您今天创建订单数量已达上限！"}
         }
-      elsif @sys_msgs_timeline.update(sys_msgs_timeline_params)
+     elsif @sys_msgs_timeline.update(sys_msgs_timeline_params)
          if @sys_msgs_timeline.status == Const::SysMsg::STATUS[:finished]
             @SysMsg = SysMsg.find(@sys_msgs_timeline.sys_msg_id)
-            @SysMsg.status = Const::SysMsg::STATUS[:finished]
-            @SysMsg.save
             @order = Order.new()
             token = params[:token].presence
             user = token && User.find_by_authentication_token(token.to_s)
@@ -72,9 +72,6 @@ class Api::SysMsgsTimelines::SysMsgsTimelinesController < ApplicationController
             format.json { render json:{status: :ok, location: @sys_msgs_timeline, id:@order.id}}
          end 
          if @sys_msgs_timeline.status == Const::SysMsg::STATUS[:discarded]
-            @SysMsg = SysMsg.find(@sys_msgs_timeline.sys_msg_id)
-            @SysMsg.status = Const::SysMsg::STATUS[:discarded]
-            @SysMsg.save
             format.json { render json:{status: :ok, location: @sys_msgs_timeline}}
          end
         
@@ -96,7 +93,7 @@ class Api::SysMsgsTimelines::SysMsgsTimelinesController < ApplicationController
   def avaliable_orders_to_add(user)
     has_added = Order.where("created_at >= ? and (bidder=? or signature=?)", Time.now.beginning_of_day, user.id, user.id).size    
     limit = 0
-    limit = 2 if user.level == 1
+    limit = 10 if user.level == 1
     limit = 20 if user.level == 2
     limit = 30 if user.level == 3
     limit = has_added + 1 if user.admin #管理员无限制
